5a00fb37c063a9db99e696e2eb008b6d04c02f69
Ala eddine zalfani <aladdin910@outlook.fr>
2024-04-30 23:35:28 +0100

    Add -- adding logo

--- app/src/main/java/com/example/touristo/Views/LoginActivity.java
+++ app/src/main/java/com/example/touristo/Views/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.touristo.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
