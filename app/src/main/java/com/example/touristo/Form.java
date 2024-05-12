f2bed64e8648638790c87d6507bfe748236d0adf
hamza <hamzazarai11@gmail.com>
2024-05-12 11:59:35 +0100

    Updated map and form

--- app/src/main/java/com/example/touristo/Form.java
+++ app/src/main/java/com/example/touristo/Form.java
@@ -2,13 +2,36 @@ package com.example.touristo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class Form extends AppCompatActivity {
-
+private Button btnSubmit,btnSkip;
+private RadioButton beaches,activities,events,sites;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        init();
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Form.this, Map.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    private void init()
+    {
+        btnSubmit=findViewById(R.id.btnSubmit);
+        btnSkip=findViewById(R.id.btnSkip);
+        beaches=findViewById(R.id.beaches);
+        activities=findViewById(R.id.activities);
+        events=findViewById(R.id.events);
+        sites=findViewById(R.id.sites);
     }
 }
--- app/src/main/java/com/example/touristo/Map.java
+++ app/src/main/java/com/example/touristo/Map.java
@@ -2,13 +2,92 @@ package com.example.touristo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Map extends AppCompatActivity {
-
+private Button bizerte ,jendouba,sousse,kairouan,kasserine,sbz,gabes,tozeur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        init();
+        bizerte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        jendouba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        sousse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        kairouan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        kasserine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        sbz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        gabes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        tozeur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    public void init()
+    {
+        bizerte = findViewById(R.id.bizerte);
+        jendouba =findViewById(R.id.jendouba);
+        sousse =findViewById(R.id.sousse);
+        kairouan =findViewById(R.id.kairouan);
+        kasserine =findViewById(R.id.kasserine);
+        sbz = findViewById(R.id.sbz);
+        gabes =findViewById(R.id.gabes);
+        tozeur =findViewById(R.id.tozeur);
     }
 }
